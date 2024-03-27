cbcbd2102ac3c17e048c7d142d8ea4a62d848bf9
tmbc12 <reachus@tmbc.in>
2024-03-27 11:35:46 +0530

    ///

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,39 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+import daisyui from 'daisyui';
+import light from 'daisyui/src/theming/themes';
+import dark from 'daisyui/src/theming/themes';
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-white': '#ffffff2b',
+        'dark-grey': '#202123',
+        'light-grey': '#353740',
+      },
+    },
+    default: 'daisy',
   },
-  plugins: [require("@tailwindcss/typography")],
+  daisyui: {
+    darkTheme: 'dark',
+    themes: [
+      'light',
+      'dark',
+      {
+        light: {
+          ...light['[data-theme=light]'],
+          neutral: '#cbd5e1',
+          'neutral-content': '#475569',
+        },
+        dark: {
+          ...dark['[data-theme=dark]'],
+          neutral: '#171c21',
+          'neutral-content': '#94a3b8',
+        },
+      },
+    ],
+  },
+  plugins: [daisyui],
 };
